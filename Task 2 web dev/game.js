f8226d04174992b354c81e021ff8bdb100111114
Sanjeev2002 <63333119+Sanjeev2002@users.noreply.github.com>
2020-05-17 22:46:29 +0530

    Add files via upload

---
+++ Task 2 web dev/game.js
@@ -0,0 +1,137 @@
+let score=document.getElementById('number');
+let score1=document.getElementById('hs');
+const canvas = document.getElementById('canvas');
+if(canvas.getContext)
+{
+const ctx = canvas.getContext('2d');
+const ball = {
+   x: 500,
+   y: 620,
+   size: 10,
+   dx: 0,
+   dy: 0,
+speed:5,
+score:0
+ };
+function drawBall()
+{
+ctx.beginPath();
+ctx.fillStyle = 'green';
+ctx.arc(ball.x, ball.y, ball.size, 0, 10);
+  ctx.fill();
+ctx.closePath();
+}
+function newPos() {
+    ball.x+=ball.dx;
+  ball.y += ball.dy;
+ball.score-=ball.dy;
+score.innerHTML='Score:'+ball.score;
+hs.innerHTML='High Score:'+ball.score;
+detectWalls();
+}
+
+function detectWalls() {
+
+  if (ball.y + ball.size > canvas.height)
+{ ball.dy *= -1; }
+if(ball.y - ball.size < 0) {
+     ctx.font='48px serif';
+ctx.strokeText('Game Over',canvas.width/2-70,canvas.height/2,[150]);  
+ball.dy = 0; }
+
+}
+
+function circle(x,y,r,startAngle,endAngle,color)
+{
+this.x=x;
+this.y=y;
+this.r=r;
+this.startAngle=startAngle;
+this.endAngle=endAngle;
+this.angvel=0.02;
+this.color=color;
+this.draw=function()
+{
+ctx.beginPath();
+ctx.arc(this.x,this.y,this.r,this.startAngle,this.endAngle,false);
+ctx.save();
+ctx.strokeStyle=this.color;
+ctx.lineWidth=20;
+ctx.stroke();
+ctx.restore();
+};
+this.update=function()
+{
+this.startAngle+=this.angvel;
+this.endAngle+=this.angvel;
+this.draw();
+};
+}
+var circle1;
+var circle2;
+var circle3;
+var circle4;
+ 
+
+function init()
+{
+
+circle1=new circle(canvas.width/2,450,60,0,Math.PI,"green");
+circle2=new circle(canvas.width/2,450,60,Math.PI,2*Math.PI,"blue");
+
+
+circle3=new circle(canvas.width/2,180,60,0,Math.PI,"green");
+circle4=new circle(canvas.width/2,180,60,Math.PI,2*Math.PI,"blue");
+}
+
+
+function animate()
+{
+ctx.clearRect(0,0,canvas.width,canvas.height);
+circle1.update();
+circle2.update();
+circle3.update();
+circle4.update();
+drawBall();
+newPos();
+requestAnimationFrame(animate);
+}
+function moveUp() {
+  ball.dy = -ball.speed;
+}
+
+function moveDown() {
+  ball.dy = ball.speed;
+}
+
+
+function keyDown(e) {
+ if (e.key === 'ArrowUp' || e.key === 'Up') {
+    moveUp();
+  } else if (e.key === 'ArrowDown' || e.key === 'Down') {
+    moveDown();
+  }
+}
+
+function keyUp(e) {
+  if (
+
+  e.key == 'Up' ||
+    e.key == 'ArrowUp' ||
+    e.key == 'Down' ||
+    e.key == 'ArrowDown'
+  ) {
+    ball.dx = 0;
+    ball.dy = 0;
+  }
+}
+
+
+
+init();
+animate();
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+}
+
